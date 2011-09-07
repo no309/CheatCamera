973d1a60f3b3db4d0317b1013eaeaaf04ccce140
ttono <hoge@hotmail.co.jp>
2011-09-07 16:52:52 +0900

    セッティング画面

--- src/com/intern/cheatcamera/CheatCameraActivity.java
+++ src/com/intern/cheatcamera/CheatCameraActivity.java
@@ -1,13 +1,39 @@
 package com.intern.cheatcamera;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * @author furyu
+ *
+ */
 public class CheatCameraActivity extends Activity {
     /** Called when the activity is first created. */
+	// commit test by ¼’J@Nq
+	// commit test by mtbr
+	private Button startBtn;// ‹U’…M‰æ–Ê‚Ö
+	private Button settingBtn;// İ’è‰æ–Ê‚Ö
+	private Button playBtn;// —V‚Ñ•û‰æ–Ê‚Ö
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        startBtn = (Button)findViewById(R.id.button1);
+        settingBtn = (Button)findViewById(R.id.button2);
+        playBtn = (Button)findViewById(R.id.button3);
     }
+    // İ’è‰æ–Ê‚Ö
+    public void onClickSetting(View v){
+    	 //Ÿ‚Ì‰æ–Ê‚É‘JˆÚ‚³‚¹‚é
+        Intent intent = new Intent();
+        intent.setClassName(
+                "com.intern.cheatcamera",
+                "com.intern.cheatcamera.MenuList");
+        startActivity(intent);
+    }
+    
 }
---
+++ src/com/intern/cheatcamera/MenuList.java
@@ -0,0 +1,56 @@
+// 設定画面
+package com.intern.cheatcamera;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.ListPreference;  
+
+public class MenuList extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+	@Override
+	public void onCreate(Bundle saveInstanceState){
+		super.onCreate(saveInstanceState);
+		addPreferencesFromResource(R.xml.preference);
+		
+		//
+		// 人物名のサマリー表示		 
+		EditTextPreference edittext_preference = (EditTextPreference)getPreferenceScreen().findPreference("person");
+		edittext_preference.setSummary(edittext_preference.getText());
+		// シャッター音のサマリー表示
+		ListPreference list_sound_preference = (ListPreference)getPreferenceScreen().findPreference("sound");
+		list_sound_preference.setSummary(list_sound_preference.getEntry());
+		// 時間のサマリー表示
+		ListPreference list_time_preference = (ListPreference)getPreferenceScreen().findPreference("time");
+		list_time_preference.setSummary(list_time_preference.getEntry());
+
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+	
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,  String key) {
+		// 人物名のサマリー表示		 
+		EditTextPreference edittext_preference = (EditTextPreference)getPreferenceScreen().findPreference("person");
+		edittext_preference.setSummary(edittext_preference.getText());
+		// シャッター音のサマリー表示
+		ListPreference list_sound_preference = (ListPreference)getPreferenceScreen().findPreference("sound");
+		list_sound_preference.setSummary(list_sound_preference.getEntry());
+		// 時間のサマリー表示
+		ListPreference list_time_preference = (ListPreference)getPreferenceScreen().findPreference("time");
+		list_time_preference.setSummary(list_time_preference.getEntry());
+
+		}
+	
+
+}
