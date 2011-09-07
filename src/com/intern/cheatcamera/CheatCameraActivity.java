5789ebc3dce0b8c6cdd0dbb16b34f60cb96c082d
ttono <hoge@hotmail.co.jp>
2011-09-07 10:14:58 +0900

    first commit

---
+++ src/com/intern/cheatcamera/CheatCameraActivity.java
@@ -0,0 +1,13 @@
+package com.intern.cheatcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatCameraActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
