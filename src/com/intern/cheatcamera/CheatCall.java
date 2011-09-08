b1a94ebb5c54c64467cbd7dc8847e36a939a6d70
ttono <hoge@hotmail.co.jp>
2011-09-08 15:26:33 +0900

    add se

--- src/com/intern/cheatcamera/CheatCall.java
+++ src/com/intern/cheatcamera/CheatCall.java
@@ -1,13 +1,22 @@
 package com.intern.cheatcamera;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 //
 public class CheatCall extends Activity {
+	
+	private MediaPlayer mp;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cheatcall);
+        Log.d("onCreate","onCreate");
+        mp = MediaPlayer.create(this,R.raw.call);
+//        mp.setLooping(true);
+        mp.start();
     }
 
 }
