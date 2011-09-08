fb92fd5ebc752554df4a9b9e0941c7a8acc7c4ca
ttono <hoge@hotmail.co.jp>
2011-09-08 14:12:57 +0900

    add bluck view add the timer screen transitions

--- src/com/intern/cheatcamera/CheatCall.java
+++ src/com/intern/cheatcamera/CheatCall.java
@@ -1,7 +1,13 @@
 package com.intern.cheatcamera;
 
 import android.app.Activity;
+import android.os.Bundle;
 //
 public class CheatCall extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cheatcall);
+    }
 
 }
